3a67e7aa9529ca80568afbe6802e1da152b5a5ae
Gia Fang <giafang16@gmail.com>
2024-02-12 18:23:47 -0800

    login, signup, get_user_data done

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import helpers
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
+from pprint import pprint
 import random
 import string
 
@@ -19,41 +22,60 @@ app = Flask(__name__)
 CORS(app)
 
 #routes
-@app.route('/', methods=['GET'])
-def signup():
+@app.route('/signup', methods=['POST'])
+def signup_route():
     query = request.args.to_dict()
     email = query['emails']
-    firstname = query['firstname']
-    lastname = query['lastname']
+    first_name = query['firstname']
+    last_name = query['lastname']
+    exercise_goal = query['exercise_goal']
+    sleep_goal = query['exercise_goal']
     
-    user_id = "what"
+    user_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
     
-    return jsonify({'user_id': user_id})
+    data = {
+        "user_id": user_id,
+        "first": first_name,
+        "last": last_name,
+        "email": email,
+        "goals": {
+            "exercise": exercise_goal,
+            "sleep": sleep_goal
+        },
+        "happiness_score": 0,
+        "journal_entry": ""
+    }
+    
+    db.collection("users").document(user_id).set(data)
+    
+    return jsonify({'exit_status': 0, 'user_id': user_id})
+
+@app.route('/login', methods=['GET'])
+def login():
+    query = request.args.to_dict()
+    email = query['emails']
+    
+    user_query = db.collection("users").where("email", "==", email).limit(1).get()
+
+    if not user_query:
+        return jsonify({'exit_status': -1})
+    else:
+        user_id = user_query[0].id
+        return jsonify({'exit_status': 0, 'user_id': user_id})
+    
+@app.route('/data', methods=['GET'])
+def get_user_data():
+    query = request.args.to_dict()
+    user_id = query['user_id']
+    
+    user_query = db.collection("users").where("user_id", "==", user_id).limit(1).get()
+    
+    if not user_query:
+        return jsonify({'exit_status': -1})
+    else:
+        user_data = user_query[0].to_dict()
+        return jsonify({'exit_status': 0, 'data': user_data})
 
 if __name__ == '__main__':
     # app.run()
-    doc_ref = db.collection("users").document("testUser")
-    doc_ref.set({"first": "Gia", "last": "Fang", "email": "giafang@gmail.com", "stepgoal": 10000})
-    doc_ref = db.collection("users").document("testUser2")
-    doc_ref.set({"first": "Alan", "last": "Turing", "email": "turing@gmail.com", "stepgoal": 10000})
-    
-    # print('Reading exercises.json')
-    # records = None
-    # with open('exercises.json', encoding="utf-8") as json_file:  
-    #     records = json.load(json_file)
-    # print('%i records read from file' % len(records))
-
-    # i = 0
-    # batch = db.batch()
-    # print('Writing records to Firestore')
-    # for record in records:
-    #     doc = db.collection('exercises').document(record['field1'])
-    #     batch.set(doc, record)
-    #     i += 1
-    #     if (i % 500 == 0):
-    #         batch.commit()
-    #         batch = db.batch()
-    #         print(i)
-    # batch.commit()
-    # print(i)
     pass
