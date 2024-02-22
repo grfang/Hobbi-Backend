6562b2607912bf7c9a3e9afdf56c3b6d8611c4ed
Gia Fang <giafang16@gmail.com>
2024-02-21 20:07:21 -0800

    sleep, fitness, and journal semi-complete
    Co-authored-by: rissern <rissern@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import helpers
 
+from google.cloud import language_v2
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
-from pprint import pprint
 import random
 import string
 
@@ -16,6 +16,11 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 load_dotenv()
+
+PROJECT_ID = "happi-415103"
+assert PROJECT_ID
+PARENT = f"projects/{PROJECT_ID}"
+SENTIMENT_SCORE_CLIENT = language_v2.LanguageServiceClient()
 
 #flask setup
 app = Flask(__name__)
@@ -31,7 +36,6 @@ def signup_route():
     exercise_goal = query['exercise_goal']
     skill = query['skill']
     equipment = query['equipment']
-    exercise_type = query['exercise_type']
     sleep_goal = query['sleep_goal']
     wakeup_time = query['wakeup_time']
     
@@ -45,8 +49,7 @@ def signup_route():
         "exercise_info": {
             "exercise_goal": exercise_goal,
             "skill": skill,
-            "equipment": equipment,
-            "exercise_type": exercise_type
+            "equipment": equipment
         },
         "sleep_info": {
             "sleep_goal": sleep_goal,
@@ -90,15 +93,83 @@ def get_user_data():
     
 @app.route('/journal', methods=['GET'])
 def journal():
-    pass
+    query = request.args.to_dict()
+    user_id = query['user_id']
+    text_content = query['entry']
+    
+    document_type_in_plain_text = language_v2.Document.Type.PLAIN_TEXT
+    
+    language_code = "en"
+    document = {
+        "content": text_content,
+        "type_": document_type_in_plain_text,
+        "language_code": language_code,
+    }
+
+    encoding_type = language_v2.EncodingType.UTF8
+
+    response = SENTIMENT_SCORE_CLIENT.analyze_sentiment(
+        request={"document": document, "encoding_type": encoding_type}
+    )
+    
+    score = response.document_sentiment.score
+    
+    data_to_update = {
+        "journal_info": {
+            "happiness_score": score,
+            "journal_entry": text_content
+        }
+    }
+    
+    db.collection("users").document(user_id).update(data_to_update)
+    
+    return jsonify({'exit_status': 0, 'score': score})
 
 @app.route('/fitness', methods=['GET'])
 def fitness():
-    pass
+    query = request.args.to_dict()
+    user_id = query['user_id']
+    body_part = query['body_part']
+    exercise_type = query['exercise_type']
+    
+    user_query = db.collection("users").where("user_id", "==", user_id).limit(1).get()
+    
+    if not user_query:
+        return jsonify({'exit_status': -1})
+    
+    skill = user_query[0].to_dict().get("exercise_info", {}).get('skill')
+    equipment = user_query[0].to_dict().get("exercise_info", {}).get('equipment')
+    
+    user_query = db.collection("exercises").where("BodyPart", "==", body_part).where("Level", "==", skill).where("Type", "==", exercise_type).where("Equipment", "in", equipment).limit(20).get()
+    
+    if not user_query:
+        return jsonify({'exit_status': -1})
+    
+    exercise_data = []
+    for doc in user_query:
+        exercise_data.append(doc.to_dict())
+        
+    return jsonify({'exit_status': 0, 'exercise_data': exercise_data})
 
 @app.route('/sleep', methods=['GET'])
 def sleep():
-    pass
+    query = request.args.to_dict()
+    user_id = query['user_id']
+    user_query = db.collection("users").where("user_id", "==", user_id).limit(1).get()
+    
+    if not user_query:
+        return jsonify({'exit_status': -1})
+    
+    awakeTime = user_query[0].to_dict().get("sleep_info", {}).get('wakeup_time')
+    wakeUpTimes = []
+    track = 2
+    for x in range(3):
+        wakeUpTimes[x] = awakeTime - (6 + (1.5 * track))
+        track -= 1
+        if wakeUpTimes[x] < 0:
+            wakeUpTimes[x] = 24 + wakeUpTimes[x]
+    
+    return jsonify({'exit_status': 0, 'wakeup_times': wakeUpTimes})
 
 if __name__ == '__main__':
     # app.run()
